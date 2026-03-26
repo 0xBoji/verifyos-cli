ffd7e252dee066c60a6ae748fb47ad3d6f9c022e
0xboji <hoangpichgoodkid@gmail.com>
2026-03-26 14:12:05 +0700

    chore: Update help banner ASCII art from block characters to line characters and adjust test assertion.

--- tests/cli_test.rs
+++ tests/cli_test.rs
@@ -99,7 +99,7 @@ fn test_help_shows_verify_os_banner() {
 
     let stdout = String::from_utf8(output.stdout).expect("stdout should be utf8");
     assert!(stdout.contains("verify-OS"));
-    assert!(stdout.contains("████"));
+    assert!(stdout.contains("|___/\\____/\\____/"));
 }
 
 #[test]
